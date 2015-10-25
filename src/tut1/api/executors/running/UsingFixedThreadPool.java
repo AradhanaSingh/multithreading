b1dbd86295b0532c6f545160926952fdf264681d
Aradhana Singh <asingh17@ncsu.edu>
2015-10-25 13:21:02 -0400

    added executor service with fixed thread pool

---
+++ src/tut1/api/executors/running/UsingFixedThreadPool.java
@@ -0,0 +1,27 @@
+package tut1.api.executors.running;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import tuts.common.LoopTaskA;
+
+// no of thread is fixed
+public class UsingFixedThreadPool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main thread starts here..");
+		
+		//creating thread pool executor service
+		ExecutorService execService = Executors.newFixedThreadPool(3);
+		
+		// context switching happens in the background
+		execService.execute(new LoopTaskA());
+		execService.execute(new LoopTaskA());
+		execService.execute(new LoopTaskA());
+		
+		System.out.println("Main thread ends here...");
+
+	}
+
+}
